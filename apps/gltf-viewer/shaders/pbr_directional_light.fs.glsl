2c9feb79152ef2bb02cddcba1b4f171b65f98ab4
Adam NAILI <a.naili@live.fr>
2020-03-05 11:35:02 +0100

    Refactor the names of the variables in the shader for more clarity and adding some comments

--- apps/gltf-viewer/shaders/pbr_directional_light.fs.glsl
+++ apps/gltf-viewer/shaders/pbr_directional_light.fs.glsl
@@ -1,25 +1,32 @@
 #version 330
-
+// INPUTS
 in vec3 vViewSpacePosition;
 in vec3 vViewSpaceNormal;
 in vec2 vTexCoords;
 
+//********** UNIFORMS ************
+// LIGHT
 uniform vec3 uLightDirection;
 uniform vec3 uLightIntensity;
 
+// BASE COLOR
 uniform sampler2D uBaseColorTexture;
 uniform vec4 uBaseColorFactor;
 
+// METALLIC ROUGHNESS
 uniform float uMetallicFactor;
 uniform float uRoughnessFactor;
 uniform sampler2D uMetallicRoughnessTexture;
 
+// EMISSIVE
 uniform sampler2D uEmissiveTexture;
 uniform vec3 uEmissiveFactor;
 
+// OCCLUSION
 uniform sampler2D uOcclusionTexture;
 uniform float uOcclusionStrength;
 
+//********** OUTPUTS ***********
 out vec3 fColor;
 
 // Constants
@@ -55,20 +62,21 @@ void main()
     vec3 V = normalize(-vViewSpacePosition);
     vec3 H = normalize(L + V);
 
-    vec4 baseColorFromTexture = SRGBtoLINEAR(texture(uBaseColorTexture, vTexCoords));
-    vec4 baseColor = baseColorFromTexture * uBaseColorFactor;
-    vec4 metallicRoughnessFromTexture = texture(uMetallicRoughnessTexture, vTexCoords);
-    float metallic = uMetallicFactor * metallicRoughnessFromTexture.b;
-    float roughness = uRoughnessFactor * metallicRoughnessFromTexture.g;
+    vec4 baseColorVectorFromTexture = SRGBtoLINEAR(texture(uBaseColorTexture, vTexCoords));
+    vec4 computedBaseColorVector = baseColorVectorFromTexture * uBaseColorFactor;
 
-    vec4 baseEmissiveFromTexture = SRGBtoLINEAR(texture(uEmissiveTexture, vTexCoords));
-    vec4 emissiveVector = baseEmissiveFromTexture * vec4(uEmissiveFactor, 0);
+    vec4 metallicRoughnessVectorFromTexture = texture(uMetallicRoughnessTexture, vTexCoords);
+    float computedMetallicValue = uMetallicFactor * metallicRoughnessVectorFromTexture.b;
+    float computedRoughnessValue = uRoughnessFactor * metallicRoughnessVectorFromTexture.g;
 
-    vec4 baseOcclusionFromTexture = texture(uOcclusionTexture, vTexCoords);
+    vec4 baseEmissiveVectorFromTexture = SRGBtoLINEAR(texture(uEmissiveTexture, vTexCoords));
+    vec4 computedEmissiveVector = baseEmissiveVectorFromTexture * vec4(uEmissiveFactor, 0);
 
-    vec3 c_diffuse = mix(baseColor.rgb * (1 - dielectricSpecular.r), black, metallic);
-    vec3 F_O = mix(dielectricSpecular, baseColor.rgb, metallic);
-    float alpha = roughness * roughness;
+    vec4 baseOcclusionVectorFromTexture = texture(uOcclusionTexture, vTexCoords);
+
+    vec3 c_diffuse = mix(computedBaseColorVector.rgb * (1 - dielectricSpecular.r), black, computedMetallicValue);
+    vec3 F_O = mix(dielectricSpecular, computedBaseColorVector.rgb, computedMetallicValue);
+    float alpha = computedRoughnessValue * computedRoughnessValue;
 
     float NdotL = clamp(dot(N, L), 0, 1);
     float NdotV = clamp(dot(N, V), 0, 1);
@@ -87,16 +95,16 @@ void main()
     vec3 F = F_O + (1 - F_O) * shlickFactor;
 
     float alphaPow2 = alpha * alpha;
-    float denominatorVis = NdotL * sqrt(NdotVpow2 * (1 - alphaPow2) + alphaPow2) + NdotV * sqrt(NdotLpow2 * (1 - alphaPow2) + alphaPow2);
+    float VisDenominator = NdotL * sqrt(NdotVpow2 * (1 - alphaPow2) + alphaPow2) + NdotV * sqrt(NdotLpow2 * (1 - alphaPow2) + alphaPow2);
     float Vis = 0;
-    if (denominatorVis > 0) {
-        Vis = 0.5 / denominatorVis;
+    if (VisDenominator > 0) {
+        Vis = 0.5 / VisDenominator;
     }
 
-    float denominatorD = M_PI * (NdotHpow2 * (alphaPow2 - 1) + 1) * (NdotHpow2 * (alphaPow2 - 1) + 1);
+    float DDenominator = M_PI * (NdotHpow2 * (alphaPow2 - 1) + 1) * (NdotHpow2 * (alphaPow2 - 1) + 1);
     float D = 0;
-    if (denominatorD > 0) {
-        D = alphaPow2 / denominatorD;
+    if (DDenominator > 0) {
+        D = alphaPow2 / DDenominator;
     }
 
     vec3 diffuse = c_diffuse / M_PI;
@@ -104,7 +112,7 @@ void main()
     vec3 f_diffuse = (1 - F) * diffuse;
     vec3 f_specular = F * Vis * D;
 
-    fColor = (f_diffuse + f_specular) * uLightIntensity * NdotL + vec3(emissiveVector);
-    fColor = mix(fColor, fColor * baseOcclusionFromTexture.r, uOcclusionStrength);
+    fColor = (f_diffuse + f_specular) * uLightIntensity * NdotL + vec3(computedEmissiveVector);
+    fColor = mix(fColor, fColor * baseOcclusionVectorFromTexture.r, uOcclusionStrength);
     fColor = LINEARtoSRGB(fColor);
 }
